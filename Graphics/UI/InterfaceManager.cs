151dec7f64db0f97a0fc8b19b958026dca53c469
Antoine Pilote <antoine@uxpertise.ca>
2020-06-05 21:44:27 -0400

    Added imgui

---
+++ Graphics/UI/InterfaceManager.cs
@@ -0,0 +1,31 @@
+﻿using ImGuiNET;
+using Microsoft.Xna.Framework;
+using MonoGame.ImGui;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloMonoGame.Graphics.UI
+{
+    public static class InterfaceManager
+    {
+        private static ImGUIRenderer GuiRenderer;
+
+        public static void Initialize(Game owner)
+        {
+            GuiRenderer = new ImGUIRenderer(owner);
+        }
+
+        public static void RenderLayout(GameTime gameTime)
+        {
+            GuiRenderer.BeginLayout(gameTime);
+
+            ImGui.NewFrame();
+            ImGui.Button("Hello");
+            ImGui.EndFrame();
+            GuiRenderer.EndLayout();
+        }
+    }
+}
